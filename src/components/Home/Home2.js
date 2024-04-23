cbab46bdec4bf58088a80221a81db0d79418c6ee
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-04-23 13:03:58 +0530

    added

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -85,16 +85,7 @@ function Home2() {
                   <FaLinkedinIn />
                 </a>
               </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
+              
             </ul>
           </Col>
         </Row>

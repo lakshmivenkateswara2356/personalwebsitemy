f103c19ff35f34898f54cc981837bca99a7556d4
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-17 17:59:34 +0530

    updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> PUNNAM LAKSHMIVENKATESWARA</strong>
+                <strong className="main-name"> PUNNAM VENKATESWARA</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
